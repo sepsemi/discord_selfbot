25491dc273dcbb474fd96bed2a5c7866646f92e1
sepsemi <sepsemi@protonmail.com>
2023-01-26 12:44:36 +0100

    cleaned up the library usage and main.py

--- main.py
+++ main.py
@@ -4,31 +4,22 @@ import asyncio
 import uvloop
 from datetime import datetime
 
-import dlib
+from dlib import Client, run_clients, logger
 
-from src.automation import background_delete
+from src.automation import background_delete, background_change_username
 from src.logger import Logger, MessageLogger
 
-
-logger = logging.getLogger('dlib')
-
-logger.setLevel(logging.INFO)
-sh = logging.StreamHandler()
-sh.setFormatter(logging.Formatter(
-    '[%(asctime)s][%(levelname)s] %(name)s - %(message)s'))
-logger.addHandler(sh)
+logger('info')
 
 with open('etc/config.toml') as fp:
     config = toml.load(fp)
-
 
 def yield_token(path):
     with open(path) as fp:
         for line in fp.readlines():
             yield line.rstrip()
 
-
-class Client(dlib.Client):
+class Client(Client):
 
     async def on_ready(self):
         ts = datetime.now()
@@ -39,12 +30,15 @@ class Client(dlib.Client):
             if self.user.id in config['settings']['automation']['user_ids']:
                 self.loop.create_task(background_delete(self))
 
+
+        if config['settings']['automation']['change_username']:
+            if self.user.id in config['settings']['automation']['user_ids']:
+                self.loop.create_task(background_change_username(self))
+
     async def on_new_user(self, ctx):
         Logger(self).new_user(ctx)
 
     async def on_message(self, ctx):
-
-        self._closed = True
         MessageLogger(self).send(ctx)
 
     async def on_message_delete(self, ctx):
@@ -53,18 +47,11 @@ class Client(dlib.Client):
     async def on_message_edit(self, before, after):
         MessageLogger(self).edit(before, after)
 
+clients = []
+for token in yield_token('etc/tokens.txt'):
+    client = Client(token=token)
+    clients.append(client)
 
-async def main(loop):
-    clients = []
-    for token in yield_token('etc/tokens.txt'):
-        client = Client(loop=loop, token=token)
-        clients.append(loop.create_task(client.run()))
+print('subscribed {} clients'.format(len(clients)))
+run_clients(*clients, reconnect=True)
 
-    print('subscribed {} clients'.format(len(clients)))
-
-    await asyncio.wait(clients)
-
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-loop.run_until_complete(main(loop))
